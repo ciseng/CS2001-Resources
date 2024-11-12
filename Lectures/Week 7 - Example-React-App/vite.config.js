73ee444532d6b51e963117bb1497a4366a047a10
ciseng <ciseng@users.noreply.github.com>
2024-11-12 13:20:42 +0000

    Update vite.config.js

--- Lectures/Week 7 - Example-React-App/vite.config.js
+++ Lectures/Week 7 - Example-React-App/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "CS2001-Resources",
+  base: "CS2001-ResourcesLectures/Week 7 - Example-React-App",
   plugins: [react()],
 })
